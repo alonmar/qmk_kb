ec0097c193b3be6be91fde83de7228395024c3f8
Alonso <alonmar3@gmail.com>
2020-09-12 17:13:35 -0500

    update config.h

--- alonmar/config.h
+++ alonmar/config.h
@@ -2,6 +2,7 @@
 This is the c configuration file for the keymap
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
+Copyright 2020 Alonso Melgar <github: alonmar>
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
